46d15e20367d837b3f3c52cc5c141289b85a4400
emielvereycken <emiel@epilog.care>
2023-11-05 18:20:01 +0100

    fix 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(to_reverse) {
+    let result = '';
+    for (let i = to_reverse.length - 1; i >= 0; i--){
+        result += to_reverse[i];
+    }
+    return result;
 };
 
 // Do not edit below this line
