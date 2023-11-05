5d4f04b2807a128559e146a97010b81d4238f3db
emielvereycken <emiel@epilog.care>
2023-11-05 18:29:10 +0100

    fix 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for (const ele of arguments) {
+        const index = array.indexOf(ele);
+        if (index > -1){
+            array.splice(index, 1);
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
