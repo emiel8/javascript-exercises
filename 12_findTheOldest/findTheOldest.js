317b77fb0c9aa25762b3787cd946ea15ebd66b72
emielvereycken <emiel@epilog.care>
2023-11-08 17:59:17 +0100

    solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(personArray) {
+    let oldestAge = 0;
+    let oldestPerson;
+    
+    personArray.forEach(person => {
+        if ('yearOfDeath' in person){
+            age = (person.yearOfDeath - person.yearOfBirth);
+        } else {
+            currentDate = new Date();
+            currentYear = currentDate.getFullYear();
+            age = (currentYear - person.yearOfBirth);
+        }
+        if (age > oldestAge){
+            oldestAge = age;
+            oldestPerson = person;
+        }
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
