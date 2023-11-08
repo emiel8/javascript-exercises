330ae94ce4a2955da5bef938fa51c38c4dfd55b9
emielvereycken <emiel@epilog.care>
2023-11-08 17:51:25 +0100

    solve 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
+    let titles = [];
+    bookArray.forEach((book) => {
+        titles.push(book.title)
+    }
 
+    );
+    return titles;
 };
 
 // Do not edit below this line
