de13d700a97b1d238df29733f8b6461582f23c82
emielvereycken <emiel@epilog.care>
2023-11-08 17:43:39 +0100

    solve 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    return cleanStr === cleanStr.split('').reverse().join('');
 };
 
 // Do not edit below this line
