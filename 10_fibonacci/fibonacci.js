e333ca5c887012cc52f8fc9026ba33488aa4f208
emielvereycken <emiel@epilog.care>
2023-11-08 17:47:53 +0100

    solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    index = Number(index);
+    if (index <= 0){
+        return 'OOPS';
+    }
+    if (index == 1 || index == 2){
+        return 1;
+    } else {
+        return fibonacci(index-1) + fibonacci(index-2);
+    }
 };
 
 // Do not edit below this line
